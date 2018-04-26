c00953b72b1dbd686ce14c16305457fc098b2409
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2018-04-26 16:28:30 +0200

    freeTicket()

--- contracts/EventTicket.sol
+++ contracts/EventTicket.sol
@@ -1,19 +1,19 @@
 pragma solidity ^0.4.18;
 contract EventTicket {
     mapping(bytes32 => bool) public claimedTickets;
-    uint16 public freeTickets;
+    uint16 public availableTickets;
     uint16 public totalSupply;
     string public name;
     constructor() public {
         totalSupply = 2;
-        freeTickets = totalSupply;
+        availableTickets = totalSupply;
         name = "TCK";
     }
     function claimTicket(bytes32 eh) public {
         require(claimedTickets[eh] == false);
-        require(freeTickets > 0);
-        freeTickets--;
-        if(freeTickets == 0){
+        require(availableTickets > 0);
+        availableTickets--;
+        if(availableTickets == 0){
             emit NoMoreTickets();
         }
         claimedTickets[eh] = true;
@@ -22,7 +22,15 @@ contract EventTicket {
     function checkTicket(bytes32 eh) public view returns (bool) {
         return claimedTickets[eh];
     }
+    function freeTicket(bytes32 eh) public {
+        require(claimedTickets[eh] == true);
+        claimedTickets[eh] = false;
+        emit TicketFreed(eh);
+        availableTickets++;
+        claimedTickets[eh] = false;
+    }
 
     event TicketClaimed(bytes32 eh);
+    event TicketFreed(bytes32 eh);
     event NoMoreTickets();
 }
